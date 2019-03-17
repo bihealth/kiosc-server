0865334bb0b915220771ec153fbe843f1a25854f
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2019-03-17 11:34:36 +0100

    Setup of Django was missing.

--- config/asgi.py
+++ config/asgi.py
@@ -1,6 +1,10 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+import django
+
 import dockerapps.urls
+
+django.setup()
 
 application = ProtocolTypeRouter(
     {

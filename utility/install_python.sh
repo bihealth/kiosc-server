b95a3dfaad5dfc8fb8cae751a5c4da2384c6d5e2
Federico Marotta <federico.marotta@bih-charite.de>
2026-02-06 16:06:46 +0100

    update docker image (#174)

--- utility/install_python.sh
+++ utility/install_python.sh
@@ -8,4 +8,3 @@ apt-get -y install software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get -y update
 apt-get -y install python3.13 python3.13-dev python3.13-venv python3.13-gdbm
-curl https://bootstrap.pypa.io/get-pip.py | python3.13

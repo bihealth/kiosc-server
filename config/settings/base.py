b3403792e0452d4f5578d69c5c730520397730c2
Federico Marotta <federico.marotta@bih-charite.de>
2026-02-27 11:09:16 +0100

    enable projectroles modify api (#178)

--- config/settings/base.py
+++ config/settings/base.py
@@ -622,7 +622,7 @@ PROJECTROLES_ALLOW_LOCAL_USERS = env.bool(
 PROJECTROLES_ALLOW_ANONYMOUS = False
 
 # Enable project modify API
-PROJECTROLES_ENABLE_MODIFY_API = False
+PROJECTROLES_ENABLE_MODIFY_API = True
 # List of apps for executing project modify API actions in the given order. If
 # not set, backend and project apps will execute in alphabetical order by name.
 PROJECTROLES_MODIFY_API_APPS = []

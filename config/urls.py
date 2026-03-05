a571d40d3bf84e4dcccfc98a38292aee8b8c3ee5
Federico Marotta <federico.marotta@bih-charite.de>
2026-03-05 17:05:46 +0100

    add LogWatcher websocket (#119)

--- config/urls.py
+++ config/urls.py
@@ -9,59 +9,63 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
 from projectroles.views import HomeView
+from containers.urls import websocket_urlpatterns
 
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path(
-        'about/',
-        TemplateView.as_view(template_name='pages/about.html'),
-        name='about',
-    ),
-    # Admin URLs - most occur before Django Admin, otherwise urls will be matched by that.
-    path('kioscadmin/', include('kioscadmin.urls')),
-    # Django Admin, use {% url 'admin:index' %}
-    path(settings.ADMIN_URL, admin.site.urls),
-    # Login and logout
-    path(
-        'login/',
-        auth_views.LoginView.as_view(template_name='users/login.html'),
-        name='login',
-    ),
-    path('logout/', auth_views.logout_then_login, name='logout'),
-    # Auth
-    path('api/auth/', include('knox.urls')),
-    # Social auth for OIDC support
-    path('social/', include('social_django.urls')),
-    # Projectroles URLs
-    path('project/', include('projectroles.urls')),
-    # Timeline URLs
-    path('timeline/', include('timeline.urls')),
-    # django-db-file-storage URLs (obfuscated for users)
-    # TODO: Change the URL to something obfuscated (e.g. random string)
-    path('CHANGE-ME/', include('db_file_storage.urls')),
-    # Background Jobs URLs
-    path('bgjobs/', include('bgjobs.urls')),
-    # Data Cache app
-    # path(r'^cache/', include('sodarcache.urls')),
-    # User Profile URLs
-    path('user/', include('userprofile.urls')),
-    # Admin Alerts URLs
-    path('adminalerts/', include('adminalerts.urls')),
-    # App Alerts URLs
-    path('appalerts/', include('appalerts.urls')),
-    # Site Info URLs
-    path('siteinfo/', include('siteinfo.urls')),
-    # Container List URLs
-    path('containerlist/', include('containerlist.urls')),
-    # API Tokens URLs
-    path('tokens/', include('tokens.urls')),
-    # Containers URLs
-    path('containers/', include('containers.urls')),
-    # Containertemplates URLs
-    path('containertemplates/', include('containertemplates.urls')),
-    # Iconify icon URLs
-    path('icons/', include('dj_iconify.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path('', HomeView.as_view(), name='home'),
+        path(
+            'about/',
+            TemplateView.as_view(template_name='pages/about.html'),
+            name='about',
+        ),
+        # Admin URLs - most occur before Django Admin, otherwise urls will be matched by that.
+        path('kioscadmin/', include('kioscadmin.urls')),
+        # Django Admin, use {% url 'admin:index' %}
+        path(settings.ADMIN_URL, admin.site.urls),
+        # Login and logout
+        path(
+            'login/',
+            auth_views.LoginView.as_view(template_name='users/login.html'),
+            name='login',
+        ),
+        path('logout/', auth_views.logout_then_login, name='logout'),
+        # Auth
+        path('api/auth/', include('knox.urls')),
+        # Social auth for OIDC support
+        path('social/', include('social_django.urls')),
+        # Projectroles URLs
+        path('project/', include('projectroles.urls')),
+        # Timeline URLs
+        path('timeline/', include('timeline.urls')),
+        # django-db-file-storage URLs (obfuscated for users)
+        # TODO: Change the URL to something obfuscated (e.g. random string)
+        path('CHANGE-ME/', include('db_file_storage.urls')),
+        # Background Jobs URLs
+        path('bgjobs/', include('bgjobs.urls')),
+        # Data Cache app
+        # path(r'^cache/', include('sodarcache.urls')),
+        # User Profile URLs
+        path('user/', include('userprofile.urls')),
+        # Admin Alerts URLs
+        path('adminalerts/', include('adminalerts.urls')),
+        # App Alerts URLs
+        path('appalerts/', include('appalerts.urls')),
+        # Site Info URLs
+        path('siteinfo/', include('siteinfo.urls')),
+        # Container List URLs
+        path('containerlist/', include('containerlist.urls')),
+        # API Tokens URLs
+        path('tokens/', include('tokens.urls')),
+        # Containers URLs
+        path('containers/', include('containers.urls')),
+        # Containertemplates URLs
+        path('containertemplates/', include('containertemplates.urls')),
+        # Iconify icon URLs
+        path('icons/', include('dj_iconify.urls')),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + websocket_urlpatterns
+)
 
 if settings.KIOSC_EMBEDDED_FILES:
     urlpatterns.append(path('files/', include('filesfolders.urls')))
